405c15f1768e33fc109a16be41802fb9143ff76b
fabiangvizcaino <fabiangvizcaino@gmail.com>
2022-12-17 17:01:56 -0300

    cambiosimagenultima

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
+from django.conf.urls.static import static
+from django.conf import settings
 from apps.post.views import *
 
 
@@ -37,5 +39,5 @@ urlpatterns = [
     path('agregar_evento/', agregarEvento, name= 'agregar_evento'),
     re_path('leer_post/(?P<id>\d+)/$', leerPost, name='posteo'),
 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
